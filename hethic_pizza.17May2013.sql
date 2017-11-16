89ddde7f700a1d00e08165d73aae10666cfa8c0c
Sean Anderson <seanka@datavirtue.com>
2017-11-16 00:02:07 -0500

    Update hethic_pizza.17May2013.sql

--- hethic_pizza.17May2013.sql
+++ hethic_pizza.17May2013.sql
@@ -1,21 +1,8 @@
--- Data model for "MyPizzeria" restaurant online order app in PHP. Model was originally designed using MySqlWorkbench.
+-- Data model for an online food order app; written in PHP. Model was originally designed using MySql Workbench.
 -- Sean K. Anderson 2012
--- MySQL dump 10.13  Distrib 5.5.23, for Linux (x86_64)
---
--- Host: localhost    Database: hethic_pizza
--- ------------------------------------------------------
--- Server version	5.5.23-55
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- MySQL Distrib 5.5.23, for Linux (x86_64)
+-- ----------------------------------------------------
 
 --
 -- Table structure for table `ci_sessions`
